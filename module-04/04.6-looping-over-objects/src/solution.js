a10b0cfd8bffb6931053f66f313c6897f95b639a
Cannon Spears <cannonspears@gmail.com>
2023-02-28 10:37:04 -0600

    Added 4.6

---
+++ module-04/04.6-looping-over-objects/src/solution.js
@@ -0,0 +1,29 @@
+
+const cart = {
+  "Gold Round Sunglasses": { quantity: 1, priceInCents: 1000 },
+  "Pink Bucket Hat": { quantity: 2, priceInCents: 1260 },
+};
+
+function calculateCartTotal(cart) {
+  let total = 0;
+  for (let i in cart) {
+    const item = cart[i]
+    const quantity = item.quantity
+    const price = item.priceInCents
+    total += quantity * price
+  } return total
+}
+
+function printCartInventory(cart) {
+  let string = ""
+  for (let name in cart) {
+    const item = cart[name]
+    const quantity = item.quantity
+    string += `${quantity}x${name}\n`
+  } return string
+}
+
+module.exports = {
+  calculateCartTotal,
+  printCartInventory,
+};
