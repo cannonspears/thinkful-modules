f774726f1d0e6774e4d5432d1ad3efdffe5d3e37
Cannon Spears <cannonspears@gmail.com>
2023-03-17 12:00:00 -0500

    Added 15.2

---
+++ module-15/15.02-promise-chaining/src/main.js
@@ -0,0 +1,18 @@
+const axios = require("../utils/axios");
+const BASE_URL = "http://localhost:5000";
+
+function updateIfExists(id, body) {
+  const url = `${BASE_URL}/constellations/${id}`;
+  return axios.get(url)
+    .then(() => {
+    return axios.put(url, body)
+  }).then(({data}) => {
+    return data
+  }).catch(({message}) => {
+    return message
+  })
+}
+
+module.exports = {
+  updateIfExists,
+};
