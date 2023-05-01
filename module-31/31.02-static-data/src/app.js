0fdb88386935bdde58e5e6b5e27050b7428aa32a
Cannon Spears <cannonspears@gmail.com>
2023-05-01 10:30:00 -0500

    Added 31.2

---
+++ module-31/31.02-static-data/src/app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+
+const users = require("./data/users-data");
+const states = require("./data/states-data");
+
+// TODO: return a single user by id from /users/:userId in form of { data: Object }
+
+app.use("/users/:userId", (req, res, next) => {
+  const { userId } = req.params;
+  const foundUser = users.find((user) => user.id === Number(userId));
+
+  if (foundUser) {
+    res.json({ data: foundUser });
+  } else {
+    next(`User ID not found: ${userId}`);
+  }
+});
+
+// TODO: return an array of users from /users in form of { data: Array }
+
+app.use("/users", (req, res) => {
+   res.json({ data: users})
+})
+
+// TODO: Return a single state from /states/:stateCode in the form of { data: { stateCode: String, name: String } }
+
+app.use("/states/:stateCode", (req, res, next) => {
+  const { stateCode } = req.params;
+  
+  if (states[stateCode]) {
+    
+    res.json({ data: { stateCode: stateCode, name: states[stateCode] } });
+  } else {
+    next(`State code not found: ${stateCode}`);
+  }
+});
+
+// TODO: return all states from /states in the form of { data: Array }
+
+app.use("/states", (req, res) => {
+   res.json({ data: states})
+})
+
+// TODO: add not-found handler
+
+app.use((req, res, next) => {
+  next(`Not found: ${req.originalUrl}`);
+});
+
+// Error handler
+app.use((error, request, response, next) => {
+  console.error(error);
+  response.send(error);
+});
+
+// TODO: Add error handler
+
+module.exports = app;
