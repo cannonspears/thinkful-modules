9fac7d6c415efa33982edf3b506083c46b8add4c
Cannon Spears <cannonspears@gmail.com>
2023-05-09 11:30:00 -0500

    Added 33.6

---
+++ module-33/33.06-inserting-data/src/artists/insert-artist.sql
@@ -0,0 +1,15 @@
+-- first_name: Kehinde
+-- last_name: Wiley
+-- birthday: 1977-02-28
+-- is_alive: true
+
+INSERT INTO artists
+  (first_name, last_name, birthday, is_alive)
+  VALUES (
+    'Kehinde',
+    'Wiley',
+    '1977-02-28',
+    true
+  )
+
+
---
+++ module-33/33.06-inserting-data/src/artists/insert-multiple-artists.sql
@@ -0,0 +1,6 @@
+INSERT INTO artists
+  (first_name, last_name, birthday, is_alive)
+  VALUES 
+    ('Kehinde','Wiley','1977-02-28',true),
+    ('Bob','Billy','1977-02-28',true),
+    ('Joe','Bennett','1977-02-28',false);
