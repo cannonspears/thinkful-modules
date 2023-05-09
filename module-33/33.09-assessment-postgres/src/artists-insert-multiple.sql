a78409f219a69f83db0244befba0bff55fb7522f
Cannon Spears <cannonspears@gmail.com>
2023-05-09 15:30:00 -0500

    Added 33.9

---
+++ module-33/33.09-assessment-postgres/src/artists-insert-multiple.sql
@@ -0,0 +1,7 @@
+insert into artists
+(artist_name, birthday, birthplace, is_alive)
+VALUES 
+  ('Alice Apple', '1999-12-11', 'Moscow, Russia', true),
+   ('Bob Billings', '1900-12-16', 'Moscow, Russia', false),
+    ('Cecil Cummings', '1899-11-16', 'Moscow, Russia', false),
+     ('Dave Dillings', '1700-12-16', 'Moscow, Russia', false)
