6f41b44fbe2236a6764fcea9b5d16a14d1d55eba
Cannon Spears <cannonspears@gmail.com>
2023-05-17 15:30:00 -0500

    Added 35.10

---
+++ module-35/35.10-assessment-node-express-and-postgres/src/comments/comments.service.js
@@ -0,0 +1,30 @@
+const knex = require("../db/connection");
+
+function list() {
+  return knex("comments")
+    .select("*")
+}
+
+function listCommenterCount() {
+  return knex("comments as c")
+    .join("users as u", "u.user_id", "c.commenter_id")
+    .select("u.user_email as commenter_email")
+    .count("u.user_email")
+    .orderBy("commenter_email")
+    .groupBy("commenter_email")
+}
+
+function read(commentId) {
+  return knex("comments as c")
+    .join("users as u", "u.user_id", "c.commenter_id")
+    .join("posts as p", "p.post_id", "c.post_id")
+    .where("c.comment_id", commentId)
+    .select("c.comment_id", "c.comment", "u.user_email as commenter_email", "p.post_body as commented_post")
+    .first()
+}
+
+module.exports = {
+  list,
+  listCommenterCount,
+  read,
+};
